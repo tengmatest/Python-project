32c60e08eae68aa80c6d57572204338a20881e8e
matengsi4184 <matengsi4184@sogou-inc.com>
2018-03-20 16:12:09 +0800

    【tengma】modify web scrapy

--- 201803-pythonScript/scrapy_keyword_demo/scrapy_keyword.py
+++ 201803-pythonScript/scrapy_keyword_demo/scrapy_keyword.py
@@ -1,44 +1,12 @@
-# -*- coding:utf-8 -*-
+# -*- coding: UTF-8 -*-
 
 import requests
 import re
 import codecs
 
-# j = 1
-#
-# html = requests.get('http://app.mi.com/topList')
-#
-# label = re.findall('<p class="app-desc">', html.text, re.S)
-#
-# pageAppNum = len(label)
-#
-# print(pageAppNum)
-#
-# appNum = 1000
-#
-# pageNum = math.ceil(float(appNum) / pageAppNum)
-#
-# # 先清空文件内容
-# fo = open('../extract_keyword_demo/appNamesByScrapy', 'w')
-# fo.truncate()
-# fo.close()
-#
-# # 抓取并写入文件
-# f = codecs.open('../extract_keyword_demo/appNamesByScrapy', 'a', 'utf-8')
-#
-# for i in range(1, int(pageNum + 1)):
-#     html = requests.get('http://app.mi.com/topList?page=' + str(i))
-#     title = re.findall('</a><h5><a href=".*?">(.*?)</a></h5><p class="app-desc">', html.text, re.S)
-#     for each in title:
-#         if j <= appNum:
-#             print(str(j) + ":" + each)
-#             f.writelines(each + '\n')
-#         j = j + 1
-#
-# f.close()
-
 j = 1
 
+# 应用汇
 pageNum = 34
 
 # 先清空文件内容
@@ -49,20 +17,27 @@ fo.close()
 # 抓取并写入文件
 f = codecs.open('../extract_keyword_demo/appNamesByScrapy', 'a', 'utf-8')
 
-for i in range(1, int(pageNum)):
-    html = requests.get('http://www.appchina.com/category/40/' + str(i) + '_1_1_3_0_0_0.html')
-    title = re.findall('<h1 class="app-name" title="(.*?)">', html.text, re.S)
-    for each in title:
-        print(str(j) + ":" + each)
-        f.writelines(each + '\n')
-        j = j + 1
-
-for i in range(1, int(pageNum)):
-    html = requests.get('http://www.appchina.com/category/30/' + str(i) + '_1_1_3_0_0_0.html')
-    title = re.findall('<h1 class="app-name" title="(.*?)">', html.text, re.S)
-    for each in title:
-        print(str(j) + ":" + each)
-        f.writelines(each + '\n')
-        j = j + 1
+# 411~424-游戏 301~315,60-软件
+for a in range(1, 15):
+    for i in range(1, int(pageNum)):
+        html401 = requests.get('http://www.appchina.com/category/' + str(410+a) + '/1_1_' + str(i) + '_1_0_0_0.html')
+        html402 = requests.get('http://www.appchina.com/category/' + str(410+a) + '/1_' + str(i) + '_1_2_0_0_0.html')
+        html403 = requests.get('http://www.appchina.com/category/' + str(410+a) + '/' + str(i) + '_1_1_3_0_0_0.html')
+        html301 = requests.get('http://www.appchina.com/category/' + str(300+a) + '/1_1_' + str(i) + '_1_0_0_0.html')
+        html302 = requests.get('http://www.appchina.com/category/' + str(300+a) + '/1_' + str(i) + '_1_2_0_0_0.html')
+        html303 = requests.get('http://www.appchina.com/category/' + str(300+a) + '/' + str(i) + '_1_1_3_0_0_0.html')
+        title = re.findall('<h1 class="app-name" title="(.*?)">', html401.text, re.S)
+        title = title + re.findall('<h1 class="app-name" title="(.*?)">', html402.text, re.S)
+        title = title + re.findall('<h1 class="app-name" title="(.*?)">', html403.text, re.S)
+        title = title + re.findall('<h1 class="app-name" title="(.*?)">', html301.text, re.S)
+        title = title + re.findall('<h1 class="app-name" title="(.*?)">', html302.text, re.S)
+        title = title + re.findall('<h1 class="app-name" title="(.*?)">', html303.text, re.S)
+        if len(title) == 0:
+            continue
+        for each in title:
+            print str(j) + ":" + each
+            f.writelines(each + '\n')
+            j = j + 1
+        print '------' + str(a) + '------' + str(i) + '-------' + str(len(title))
 
 f.close()
